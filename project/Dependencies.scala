756f7f4b6af1ac2ee0f269056bc1940876baf67d
Patric Fornasier <patric.fornasier@gmail.com>
2013-01-17 09:08:40 +0000

    pat: added json support

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,10 +1,12 @@
 import sbt._
 
 object Dependencies {
+    val ScalatraVersion = "2.2.0-RC3"
+
     val dependencies = Seq(
-        "org.scalatra" % "scalatra" % "2.2.0-RC3" cross CrossVersion.binary,
-        "org.scalatra" % "scalatra-json" % "2.2.0-RC3",
-        "org.json4s" %% "json4s-jackson" % "3.1.0" exclude("org.scala-lang", "scalap"),
+        "org.scalatra" %% "scalatra" % ScalatraVersion, 
+        "org.scalatra" %% "scalatra-json" % ScalatraVersion, 
+        "org.json4s" %% "json4s-jackson" % "3.1.0",
         "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
         "org.eclipse.jetty" % "jetty-webapp" % "8.1.7.v20120910" % "compile;container",
         "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "compile;container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar")))
--- src/main/scala/ambient/api/skeleton/SkeletonController.scala
+++ src/main/scala/ambient/api/skeleton/SkeletonController.scala
@@ -1,16 +1,16 @@
 package ambient.api.skeleton
 
 import org.scalatra._
-//import org.json4s.{DefaultFormats, Formats}
-//import org.scalatra.json._
+import org.scalatra.json._
+import org.json4s.{DefaultFormats, Formats}
 
-class SkeletonController extends ScalatraServlet { //with JacksonJsonSupport {
+class SkeletonController extends ScalatraServlet with JacksonJsonSupport {
 
-//  protected implicit val jsonFormats: Formats = DefaultFormats
-//
-//  before() {
-//    contentType = formats("json")
-//  }
+  implicit protected val jsonFormats: Formats = DefaultFormats
+
+  before() {
+    contentType = formats("json")
+  }
 
   get("/") {
     List(Skeleton("Jack Skellington", "Pumpkin King"), Skeleton("Oogie Boogie", "Bogeyman"), Skeleton("Dr. Finkelstein", "Mad Scientist"))
