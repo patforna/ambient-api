7b48641c45786b04787490fe27c6df19bc1b8db3
Patric Fornasier <patric.fornasier@gmail.com>
2013-01-25 11:40:46 +0000

    pat: added sources for dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,15 +4,15 @@ object Dependencies {
     val ScalatraVersion = "2.2.0-RC3"
 
     val dependencies = Seq(
-        "org.scalatra" %% "scalatra" % ScalatraVersion, 
-        "org.scalatra" %% "scalatra-json" % ScalatraVersion, 
-        "org.json4s" %% "json4s-jackson" % "3.1.0",
-        "org.json4s" %% "json4s-mongo" % "3.1.0",
+        "org.scalatra" %% "scalatra" % ScalatraVersion withSources(), 
+        "org.scalatra" %% "scalatra-json" % ScalatraVersion withSources(), 
+        "org.json4s" %% "json4s-jackson" % "3.1.0" withSources(),
+        "org.json4s" %% "json4s-mongo" % "3.1.0" withSources(),
         "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
-        "org.eclipse.jetty" % "jetty-webapp" % "8.1.7.v20120910" % "compile;container",
-        "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "compile;container;test" artifacts (Artifact("javax.servlet", "jar", "jar")),
+        "org.eclipse.jetty" % "jetty-webapp" % "8.1.7.v20120910" % "compile;container" withSources(),
+        "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "compile;container;test" artifacts (Artifact("javax.servlet","jar", "jar")) withSources(),
         "org.mongodb" %% "casbah" % "2.5.0",
-        "org.scalatest" %% "scalatest" % "1.9.1" ,
-        "org.scalatra" %% "scalatra-scalatest" % ScalatraVersion % "test"
+        "org.scalatest" %% "scalatest" % "1.9.1" withSources,
+        "org.scalatra" %% "scalatra-scalatest" % ScalatraVersion % "test" withSources()
     )
 }
