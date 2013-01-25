22d65231a7073e576167e02b7808456a27f730a9
Patric Fornasier <patric.fornasier@gmail.com>
2013-01-25 15:12:58 +0000

    pat: fixed typo

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
         "org.eclipse.jetty" % "jetty-webapp" % "8.1.7.v20120910" % "compile;container" withSources(),
         "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "compile;container;test" artifacts (Artifact("javax.servlet","jar", "jar")) withSources(),
         "org.mongodb" %% "casbah" % "2.5.0",
-        "org.scalatest" %% "scalatest" % "1.9.1" withSources,
+        "org.scalatest" %% "scalatest" % "1.9.1" withSources(),
         "org.scalatra" %% "scalatra-scalatest" % ScalatraVersion % "test" withSources(),
         "org.mockito" % "mockito-core" % "1.9.5" % "test"
     )
