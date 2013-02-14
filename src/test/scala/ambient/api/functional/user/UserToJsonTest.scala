6c386411a5521d162f04bd3474ead78a68f3be94
Patric Fornasier <patric.fornasier@gmail.com>
2013-02-14 18:45:22 +0000

    pat: test for user json serialisation

---
+++ src/test/scala/ambient/api/functional/user/UserToJsonTest.scala
@@ -0,0 +1,25 @@
+package ambient.api.functional.user
+
+import org.scalatest.FunSpec
+import org.scalatest.matchers.ShouldMatchers
+
+import ambient.api.functional.JsonHelpers._
+import ambient.api.user.User
+import ambient.api.location.Location
+
+class UserToJsonTest extends FunSpec with ShouldMatchers {
+
+  it("should serialise all user fields") {
+    val id = "1"
+    val first = "foo"
+    val last = "bar"
+    val fbid = "42"
+    val location = Location(1.1, 2.2)
+
+    val user = User(Some(id), first, last, Some(fbid), Some(location));
+
+    serialize(user) should be(json(s""" { "id" : "$id", "first" : "$first", "last" : "$last", "fbid" : "$fbid", "location" : [1.1, 2.2] } """))
+  }
+
+}
+
